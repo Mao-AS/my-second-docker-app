e75169bfb6de0bb2d8cfc711ee6dfa6df2b7bd40
Sondre Benjamin Aasen <699902+sobeaa@users.noreply.github.com>
2025-11-05 10:38:05 +0100

    feat: implemented background worker to handle our queue jobs

--- worker/src/jobs/task.job.ts
+++ worker/src/jobs/task.job.ts
@@ -1,6 +1,34 @@
 import type {Job} from 'bullmq';
-import {Task} from "../types/task.type";
+import {TaskRepository} from "../repositories/TaskRepository";
+import {CreateTaskDTO, Task, TaskID, UpdateTaskDTO} from "../types/task.js";
 
-export default async function taskJob(job: Job<Task>) {
-  // TODO: handle the task job
+export default async function taskJob(job: Job<CreateTaskDTO | UpdateTaskDTO | TaskID>) {
+  switch (job.name) {
+    case 'task:create': {
+      const data = job.data as CreateTaskDTO;
+
+      await TaskRepository.create(data);
+
+      return `Task created with data: ${JSON.stringify(data)}`;
+    }
+
+    case 'task:update': {
+      const {id, ...data} = job.data as Task;
+
+      await TaskRepository.update(id, data as UpdateTaskDTO);
+
+      return `Task with ID ${id} updated with data: ${JSON.stringify(data)}`;
+    }
+
+    case 'task:delete': {
+      const id = job.data as TaskID;
+
+      await TaskRepository.delete(id);
+
+      return `Task with ID ${id} deleted`;
+    }
+
+    default:
+      throw new Error(`Unknown job name: ${job.name}`);
+  }
 }
--- worker/src/listeners/task.listener.ts
+++ worker/src/listeners/task.listener.ts
@@ -2,6 +2,7 @@ import {Worker, type Job} from 'bullmq';
 import {redis} from '../redis';
 import taskJob from '../jobs/task.job';
 
+const IS_DEBUG_ENABLED = true;
 const QUEUE_NAME = 'tasks';
 
 export default async function listenToTasks() {
@@ -9,24 +10,30 @@ export default async function listenToTasks() {
     connection: redis,
   });
 
-  worker.on('ready', () => {
-    // TODO: on task ready
-  });
+  if (IS_DEBUG_ENABLED) {
+    worker.on('ready', () => {
+      console.log('Task worker is ready');
+    });
 
-  worker.on('active', (job: Job) => {
-    // TODO: on task active
-  });
+    worker.on('active', (job: Job) => {
+      console.log(`Processing task job #${job.id} with name ${job.name}`);
+    });
 
-  worker.on('completed', (job: Job, result: unknown) => {
-    // TODO: on task completed
-  });
+    worker.on('completed', (job: Job, result: unknown) => {
+      console.log(`Completed task job #${job.id} with name ${job.name}. Result:`, result);
+    });
+  }
 
   worker.on('failed', (job: Job | undefined, err: Error) => {
-    // TODO: on task failed
+    if (job) {
+      console.error(`Task job #${job.id} with name ${job.name} failed. Error:`, err);
+    } else {
+      console.error('A task job failed before it could be processed. Error:', err);
+    }
   });
 
   worker.on('error', (err: Error) => {
-    // TODO: on task error
+    console.error('Worker error:', err);
   });
 
   return worker;
